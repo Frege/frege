d3db61aabb61fe7dff3d298fb29625bdecfb230c
ingo <ingo@53b5f811-8420-8aa4-35af-336720f271cf>
2011-04-15 21:32:30 +0000

    added Prelude Properties
    git-svn-id: http://frege.googlecode.com/svn@72 53b5f811-8420-8aa4-35af-336720f271cf

--- mkmk.pl
+++ mkmk.pl
@@ -7,7 +7,7 @@ while (<>) {
     m/^JAVAC5/ and do { print "JAVAC5 = echo no java5 here\n"; next };
     s/pbyacc/byaccj/;
     # s/\bMakefile\b/frege.mk/;
-    s/\.;stage1/.:stage1/;
+    s/\.;build/.:build/;
     print;
 }
     
