5a72a4f9b018daeaf4fc0dba18454db15b528b38
ingo <ingo@53b5f811-8420-8aa4-35af-336720f271cf>
2011-05-15 14:09:52 +0000

    try to make mkdist.pl work under Windows
    git-svn-id: http://frege.googlecode.com/svn@125 53b5f811-8420-8aa4-35af-336720f271cf

--- mkdist.pl
+++ mkdist.pl
@@ -1,1 +1,11 @@
+#!perl
 
+use strict;
+use warnings;
+use Find::File;
+
+mkdir "dist" unless -d "dist"; 
+system q{find.exe build/frege -name "*.java" -exec rm.exe "{}" ";"};
+
+my $version = qx{java.exe -cp build frege.compiler.Main -version}
+print "making dist for version: '$version'\n"
