7a6fec3fe1880912e6ab78f1f273c5dd6dd2e32e
ingo.wechsung@googlemail.com <ingo.wechsung@googlemail.com@53b5f811-8420-8aa4-35af-336720f271cf>
2011-10-29 16:20:07 +0000

    new code gen: implemented sum types
    git-svn-id: http://frege.googlecode.com/svn@248 53b5f811-8420-8aa4-35af-336720f271cf

--- frege.bat
+++ frege.bat
@@ -7,6 +7,6 @@ setLocal
 
 if "%FREGE_HOME%" == "" (set FREGE_HOME=build)
 
-java -cp "%FREGE_HOME%;build" "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+java -cp "%FREGE_HOME%;build" %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 endlocal
