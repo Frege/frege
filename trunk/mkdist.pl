afe9b0a79973b44b8262ff39a3e32fcc75d747f8
ingo.wechsung@googlemail.com <ingo.wechsung@googlemail.com@53b5f811-8420-8aa4-35af-336720f271cf>
2012-01-22 11:41:12 +0000

    Adjusted new path to dist directory.
    git-svn-id: http://frege.googlecode.com/svn@464 53b5f811-8420-8aa4-35af-336720f271cf

--- trunk/mkdist.pl
+++ trunk/mkdist.pl
@@ -11,5 +11,5 @@ my $version = qx{java.exe -cp build frege.compiler.Main -version};
 chomp $version;
 $version =~ s/\s//g;
 print "making dist for version: '$version'\n";
-system qq{jar.exe -cfe dist/frege$version.jar frege.compiler.Main  -C build frege};
-system qq{jar.exe -cf  dist/htmldocs$version.zip doc/frege doc/*.html doc/*.css doc/package-list};
+system qq{jar.exe -cfe ../dist/frege$version.jar frege.compiler.Main  -C build frege};
+system qq{jar.exe -cf  ../dist/htmldocs$version.zip doc/frege doc/*.html doc/*.css doc/package-list};
