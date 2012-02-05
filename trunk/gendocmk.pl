f6ca8db883ee6911c7f2615f2ef8bdc7e91028d1
ingo.wechsung@googlemail.com <ingo.wechsung@googlemail.com@53b5f811-8420-8aa4-35af-336720f271cf>
2012-02-05 11:11:19 +0000

    added script to make the documentation.
    git-svn-id: http://frege.googlecode.com/svn@504 53b5f811-8420-8aa4-35af-336720f271cf

---
+++ trunk/gendocmk.pl
@@ -0,0 +1,38 @@
+#!perl -w
+
+#
+#   Create a makefile to make the documentation
+#   Run this script from the directory where the main Makefile
+#   is located, then run the makefile
+#
+
+use warnings;
+use strict;
+
+open(my $in, qq{find frege -name "*.fr" -print|})
+    or die "Can't run find: $!";
+
+my @all = ();
+
+while (<$in>) {
+    chomp;
+    next if m{/(compiler|contrib)/};
+    next if m{^frege/(Scrap|StandardLibrary|PreludeProperties)\.fr$};
+    my $fr = $_;
+    my $html = $fr;
+    $html =~ s/\.fr$/.html/;
+    $html = "doc/$html";
+    my $pack = $fr;
+    $pack =~ s/\.fr//;
+    $pack =~ s{/}/./g;
+    my $class = $fr;
+    $class =~ s/\.fr$/.class/;
+    $class = "build/$class";
+    print "$class: $fr\n";
+    print "\tjava -Xss1m -cp build frege.compiler.Main -d build -make $pack\n";
+    print "$html: $class\n";
+    print "\tjava -Xss1m -cp build frege.tools.Doc -d doc $pack\n";
+    push @all, $html;
+}
+print "all: ", join(" ", @all), "\n";
+print "\techo Documentation up to date\n";
