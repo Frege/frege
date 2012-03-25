4423dc17073b56d7f0491b1013855a8c6463a93b
jamesdouglas@gmail.com <jamesdouglas@gmail.com@53b5f811-8420-8aa4-35af-336720f271cf>
2012-03-25 21:29:07 +0000

    remove '.exe' from commands in mkdist.pl
    git-svn-id: http://frege.googlecode.com/svn@581 53b5f811-8420-8aa4-35af-336720f271cf

--- trunk/mkdist.pl
+++ trunk/mkdist.pl
@@ -5,11 +5,11 @@ use warnings;
 
 
 mkdir "../dist" unless -d "../dist";
-system q{find.exe build/frege -name "*.java" -exec rm.exe "{}" ";"};
+system q{find build/frege -name "*.java" -exec rm "{}" ";"};
 
-my $version = qx{java.exe -cp build frege.compiler.Main -version};
+my $version = qx{java -cp build frege.compiler.Main -version};
 chomp $version;
 $version =~ s/\s//g;
 print "making dist for version: '$version'\n";
-system qq{jar.exe -cfe ../dist/frege$version.jar frege.compiler.Main  -C build frege};
-system qq{jar.exe -cf  ../dist/htmldocs$version.zip doc/frege doc/*.html doc/*.css doc/package-list};
+system qq{jar -cfe ../dist/frege$version.jar frege.compiler.Main  -C build frege};
+system qq{jar -cf  ../dist/htmldocs$version.zip doc/frege doc/*.html doc/*.css doc/package-list};
