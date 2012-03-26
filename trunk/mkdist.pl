bbeab4d5bbaf48a96b253ebe8cbdad2ff55cadd2
jamesdouglas@gmail.com <jamesdouglas@gmail.com@53b5f811-8420-8aa4-35af-336720f271cf>
2012-03-26 01:23:14 +0000

    move dist artifacts from ../dist to ./dist to keep them in context
    git-svn-id: http://frege.googlecode.com/svn@582 53b5f811-8420-8aa4-35af-336720f271cf

--- trunk/mkdist.pl
+++ trunk/mkdist.pl
@@ -4,12 +4,12 @@ use strict;
 use warnings;
 
 
-mkdir "../dist" unless -d "../dist";
+mkdir "dist" unless -d "dist";
 system q{find build/frege -name "*.java" -exec rm "{}" ";"};
 
 my $version = qx{java -cp build frege.compiler.Main -version};
 chomp $version;
 $version =~ s/\s//g;
 print "making dist for version: '$version'\n";
-system qq{jar -cfe ../dist/frege$version.jar frege.compiler.Main  -C build frege};
-system qq{jar -cf  ../dist/htmldocs$version.zip doc/frege doc/*.html doc/*.css doc/package-list};
+system qq{jar -cfe dist/frege$version.jar frege.compiler.Main  -C build frege};
+system qq{jar -cf  dist/htmldocs$version.zip doc/frege doc/*.html doc/*.css doc/package-list};
