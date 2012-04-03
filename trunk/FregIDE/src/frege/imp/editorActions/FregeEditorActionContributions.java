9c7031b39aa15f2dedbfa6842feac2be8f02b169
Ingo.Wechsung@googlemail.com <Ingo.Wechsung@googlemail.com@53b5f811-8420-8aa4-35af-336720f271cf>
2012-04-03 07:29:38 +0000

    fix the toolbar buttons to appear only once
    git-svn-id: http://frege.googlecode.com/svn@608 53b5f811-8420-8aa4-35af-336720f271cf

--- trunk/FregIDE/src/frege/imp/editorActions/FregeEditorActionContributions.java
+++ trunk/FregIDE/src/frege/imp/editorActions/FregeEditorActionContributions.java
@@ -3,6 +3,8 @@ package frege.imp.editorActions;
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.imp.services.ILanguageActionsContributor;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ActionContributionItem;
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IStatusLineManager;
 import org.eclipse.jface.action.IToolBarManager;
@@ -10,18 +12,31 @@ import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
 // import org.eclipse.ui.IFileEditorInput;
 
 public class FregeEditorActionContributions implements
 		ILanguageActionsContributor {
 	
-	private Action fgAction(final UniversalEditor editor) {
+	private boolean toolbardone = false;
+	
+	private Action fgAction(final UniversalEditor uditor) {
 		Action it = new Action("f • g") {
 			public void run() {
+				UniversalEditor editor = uditor;
+				if (editor == null) {
+					IEditorPart ed = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+					if (ed == null) return;
+					if (ed instanceof UniversalEditor)
+						editor = (UniversalEditor) ed;
+					else return;
+				}
+				// editor is not null
 				try {
 					if (!editor.isEditable()) return;
 					final Point where = editor.getSelection();
-					System.err.println("we are at " + where);
+					// System.err.println("we are at " + where);
 			        final IDocument document= editor.
 			        		getDocumentProvider().
 			        		getDocument(editor.getEditorInput());
@@ -31,21 +46,33 @@ public class FregeEditorActionContributions implements
 				} 
 			    catch (BadLocationException e) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+					// e.printStackTrace();
 				}
 			}
 		};
 		it.setDescription("Insert the • symbol for the function composition operator.");
+		it.setToolTipText(it.getDescription());
+		it.setId("frege-editoraction-bullet");
 		return it;
 	}
 	
-	private Action rgxAction(final UniversalEditor editor) {
+	private Action rgxAction(final UniversalEditor uditor) {
 		Action it = new Action("´regex´") {
 			public void run() {
+				UniversalEditor editor = uditor;
+				if (editor == null) {
+					IEditorPart ed = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+					if (ed == null) return;
+					// System.err.println(ed.getClass().getName());
+					if (ed instanceof UniversalEditor)
+						editor = (UniversalEditor) ed;
+					else return;
+				}
+				// editor is not null
 				try {
 					if (!editor.isEditable()) return;
 					final Point where = editor.getSelection();
-					System.err.println("we are at " + where);
+					// System.err.println("we are at " + where);
 			        final IDocument document= editor.
 			        		getDocumentProvider().
 			        		getDocument(editor.getEditorInput());
@@ -61,6 +88,8 @@ public class FregeEditorActionContributions implements
 			}
 		};
 		it.setDescription("Enclose the selected text in grave accent marks.");
+		it.setToolTipText(it.getDescription());
+		it.setId("frege-editoraction-rgx");
 		return it;
 	}
 
@@ -84,7 +113,12 @@ public class FregeEditorActionContributions implements
 
 	public void contributeToToolBar(UniversalEditor editor,
 			IToolBarManager toolbarManager) {
-		toolbarManager.add(fgAction(editor));
-		toolbarManager.add(rgxAction(editor));
+		
+		Action fg = fgAction(null);
+		if (toolbarManager.find(fg.getId()) == null)
+			toolbarManager.add(fg);
+		Action rgx = rgxAction(null);
+		if (toolbarManager.find(rgx.getId()) == null)
+			toolbarManager.add(rgx);
 	}
 }
