88169d285e6e9758723e6023fe1d945750b5a33e
Ingo Wechsung <ingo.wechsung@gmail.com>
2012-12-06 17:36:37 +0100

    added further code to Runtime.java

--- frege/runtime/Runtime.java
+++ frege/runtime/Runtime.java
@@ -35,6 +35,13 @@
 
 package frege.runtime;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+
 /**
  * Miscellanous functions and methods needed in frege.
  * 
@@ -53,6 +60,95 @@ public class Runtime {
 	final public static int constructor(Object v) { 	// if v is completely unknown, it could still be a Value
 		return v instanceof Value ? ((Value)v)._constructor() : 0; 
 	}
+	
+    /**
+     * <p> Utility method used by <code>String.show</code> to quote a string. </p>
+     */
+    final public static java.lang.String quoteStr(java.lang.String a) {
+		java.lang.StringBuilder sr = new java.lang.StringBuilder(2+((5*a.length()) >> 2));
+		sr.append('"');
+		int i = 0;
+		char c;
+		while (i<a.length()) {
+			c = a.charAt(i++);
+			if (c<' ' || c == '\177') {
+				sr.append('\\');
+				sr.append(java.lang.String.format("%o", (int) c));
+			}
+			else if (c == '\\' || c == '"') {
+				sr.append('\\');
+				sr.append(c);
+			}
+			else sr.append(c);
+		}
+		sr.append('"');
+		return sr.toString();
+	}
+
+    /**
+     * <p> Utility method used by <code>Char.show</code> to quote a character. </p>
+     */
+	final public static java.lang.String quoteChr(char c) {
+        java.lang.StringBuilder sr = new java.lang.StringBuilder(8);
+        sr.append("'");
+        if (c<' ' || c == '\177') {
+            sr.append('\\');
+            sr.append(java.lang.String.format("%o", (int) c));
+        }
+        else if (c == '\\' || c == '\'') {
+            sr.append('\\');
+            sr.append(c);
+        }
+        else sr.append(c);
+        sr.append("'");
+        return sr.toString();
+    }
+
+	/***
+	 * This is an identity function that can be used to change phantom types, e.g.
+	 * 
+	 * <pre><code>
+	 * pure native freeze frege.runtime.Runtime.our :: Mutable s a -> a
+	 * </code></pre>
+	 * 
+	 * Often used to change a mutable value to an immutable one without copying,
+	 * this is safe if the mutable value cannot be accessed anymore, as in
+	 * the array fromList functions - here, a new mutable array is created locally and
+	 * filled with elements from a list, and then the same array object is returned
+	 * as immutable.mi-25-aug
+	 * 
+	 * 
+	 * @param o some value
+	 * @return the value passed as argument, but Frege thinks it is different.
+	 */
+    public final static<T> T our(final T o) { return o; }
+
+    /**
+     * <p> This is used to copy a mutable value. </p>
+     * <p> The returned value is of same type, but is considered immutable.
+     * Suitable for instances of classes that do not implement {@link Cloneable}, 
+     * but do implement {@link Serializable}.</p> 
+     * @param o the value to copy
+     * @return a copy obtained by serialization of the argument value, followed by deserialization
+     */
+    public final static<T extends Serializable> T copySerializable(final T o) {
+        try {
+            final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            final ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(o);
+            oos.close();
+            final ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+            final ObjectInputStream ois = new ObjectInputStream(bais);
+            @SuppressWarnings("unchecked")
+            final T s = (T) ois.readObject();
+            ois.close();
+            return s;
+        } catch (Exception exc) {
+            new Undefined("error in (de)serialization", exc).die();
+            return null;
+        }
+    }
+
 	/**
 	 * @param args
 	 */
