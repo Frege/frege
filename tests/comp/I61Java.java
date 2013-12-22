e2fdeeabe2f34b5f2230e16a426ba97311f64f02
Ingo Wechsung <ingo.wechsung@gmail.com>
2013-12-22 14:02:18 +0100

    fix Issue#61

---
+++ tests/comp/I61Java.java
@@ -0,0 +1,12 @@
+package tests.comp;
+
+/**
+ * <p>Test calss to demonstrate that $ in method names and member names work.</p>
+ * @author ingo
+ *
+ */
+final public class I61Java {
+	final public int mem$1;
+	final public int $getMem1$() { return mem$1;}
+	public I61Java(int arg) { mem$1 = arg; }
+}
