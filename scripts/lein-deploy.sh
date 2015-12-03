c934060fc954329f9a631cbe726e443cb0171ed7
Sean Corfield <sean@corfield.org>
2015-12-02 23:09:49 -0800

    Deploy to Sonatype successfully #240

--- scripts/lein-deploy.sh
+++ scripts/lein-deploy.sh
@@ -18,26 +18,25 @@ fi
 
 frege_version=`java -jar fregec.jar -version`
 echo "Looks like you have Frege version $frege_version..."
-
+frege_major=`echo $frege_version | sed 's;-.*;;'`
 # build project.clj:
-( echo "(defproject org.frege-lang/frege \"$frege_version\"";
+( echo "(defproject org.frege-lang/frege \"${frege_major}-SNAPSHOT\"";
   echo ":description \"Frege Compiler and Runtime\"";
   echo ":url \"https://github.com/Frege/frege\"";
   echo ":license {:name \"BSD 3-clause\"";
-  echo "          :url \"http://opensource.org/licenses/BSD-3-Clause\"})" ) > project.clj
+  echo "  :url \"http://opensource.org/licenses/BSD-3-Clause\"}";
+  echo ":deploy-repositories {\"snapshots\"";
+  echo "  {:url \"https://oss.sonatype.org/content/repositories/snapshots/\" :creds :gpg}}";
+  echo ")" ) > project.clj
 echo ""
 echo "Here's the project.clj file I built:"
 cat project.clj
-if test ! -d target
-then
-    mkdir target
-fi
-cp fregec.jar target/frege-${frege_version}.jar
 
-# deploy to clojars:
+# deploy to Sonatype:
 echo ""
-echo "Attempting to deploy to Clojars..."
-lein deploy clojars
+echo "Attempting to deploy to Sonatype Snapshots..."
+lein pom
+lein deploy snapshots org.frege-lang/frege ${frege_major}-SNAPSHOT fregec.jar pom.xml
 
 # cleanup:
-rm -rf pom.xml pom.xml.asc project.clj target
+rm -rf pom.xml pom.xml.asc project.clj
