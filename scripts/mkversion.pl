2f5b687c28aca3c9d545c16a928db812f02258ef
Ingo Wechsung <ingo.wechsung@gmail.com>
2016-03-06 11:29:55 +0100

    removed commit hash from version

--- scripts/mkversion.pl
+++ scripts/mkversion.pl
@@ -4,23 +4,19 @@
 use strict;
 use warnings;
 
-#
-# on that damn Windows SUA it insists on having *.exe
-# Why can't a giant like Microsoft not have a sound
-# POSIX-ish subsystem and a decent shell?
-# Its the same sh*t like in Windows 95.
-# They've done barely anything in almost 20 years.
-#
-my $exe = "";
-$exe = ".exe" unless defined $ENV{SHELL} and length $ENV{SHELL} > 0;
 
 my $version = qx{git describe --long};
+my $commit = qx{git log -n 1};
 chomp $version;
-# $version =~ s/-(\d+)-.*$/.$1/;
-$version =~ s/-/./;		# replace first dash with dot
+chomp $commit;
+$version =~ s/-(\d+)-.*$/.$1/;
+$commit =~ s/\n/\\n/g;
+$commit =~ s/\r//g;
+$commit =~ s/"/\\"/g;
 print <<XXX;
 -- automatically created with $0
 -- based on git describe
 package frege.Version where
     version = "$version"
+    commit  = "$commit"
 XXX
