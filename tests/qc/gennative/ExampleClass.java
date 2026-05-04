2db09fd7a2a788f76b2ec0b575968d474d5f6dd0
tobias.wyss <tobias.wyss@unic.com>
2026-05-04 15:24:06 +0200

    Automation of Foreign Function (aka Native) Interface - Add tests for JTypes - Add tests for JTypeQualification - Add tests for JTypeConversion - Add tests for TypeExtraction - Update tests for SigmaConversion
    Ref #354

---
+++ tests/qc/gennative/ExampleClass.java
@@ -0,0 +1,36 @@
+package tests.qc.gennative;
+
+import java.util.List;
+
+public class ExampleClass<T extends java.lang.Comparable<T>> {
+
+  public ExampleClass(T value) throws Exception {}
+
+  public String getValue() {
+    return "";
+  }
+
+  public List<? extends Number> getWildcardList() {
+    return List.of();
+  }
+  public List getRawList() {
+     return List.of();
+  }
+
+  public void setValue(T value) { }
+
+  public <E extends Number> void processParameterizedType(List<E> value) throws Exception { }
+
+  public List<String>[] getArrayOfLists() {
+      return (List<String>[]) new List<?>[3];
+  }
+
+  public int[] getIntArray() {
+      return new int[]{0,1};
+  }
+
+  public static int getInt() {
+    return 0;
+  }
+}
+
